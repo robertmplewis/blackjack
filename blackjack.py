820b8eedeb1c8fd0aaf8e934c289812a3d75a267
Robert Lewis <robertmplewis@gmail.com>
2014-11-03 16:38:39 -0800

    Created Hand Class
    created Hand Class, added import random so i can pop cards into players
    hand

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+import random
 
 def main():
 	deck = Deck()
 	deck.populate_deck()
-	print deck.cards
+	random.shuffle(deck.cards)
+	players_hand = Hand()
+	players_hand.cards.append(deck.cards.pop())
+	players_hand.cards.append(deck.cards.pop())
+	print players_hand.cards
 
 class Deck(object):
 	cards = []
@@ -20,8 +25,15 @@ class Card(object):
 	def __repr__(self):
 		return self.number + self.suit
 
+class Hand(object):
+	cards = []
+	def __init__(self):
+		pass
+
+
 card_numbers = ["A", '2', '3', '4', '5', '6', '7', '8', '9', '10', "J", "Q", "K"]
 card_suits = ["H", "C", "D", "S"]
+
 	
 if __name__ == "__main__":
 	main()
